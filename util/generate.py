9079c54b2beed264636b67eb80628dcc7d0f5c73
WenH <2018202069@ruc.edu.cn>
2023-07-10 09:35:41 +0800

    Update generate.py

--- util/generate.py
+++ util/generate.py
@@ -74,6 +74,9 @@ def generate_interactive(
         print()
 
 
+# from torch.cuda.amp import autocast  # for automatic mixed precision
+from transformers import AutoModelForCausalLM, AutoTokenizer,LlamaTokenizer
+
 def generate_fast(
     model: AutoModelForCausalLM,
     tok: AutoTokenizer,
@@ -81,78 +84,120 @@ def generate_fast(
     n_gen_per_prompt: int = 1,
     top_k: int = 5,
     max_out_len: int = 200,
-):
+) -> List[str]:
     """
-    Fast, parallelized auto-regressive text generation with top-k sampling.
-    Our custom implementation.
+    Fast, parallelized auto-regressive text generation with top-k sampling using Hugging Face's generate method.
     """
-
     # Unroll prompts and tokenize
+    if 'gpt-j' not in tok.name_or_path:
+        tok = LlamaTokenizer.from_pretrained(tok.name_or_path,padding_side="left")
+        tok.pad_token = '<unk>'
+    else:
+        tok = AutoTokenizer.from_pretrained(tok.name_or_path,padding_side="left")
+        tok.pad_token = tok.eos_token
     inp = [prompt for prompt in prompts for _ in range(n_gen_per_prompt)]
-    inp_tok = tok(inp, padding=True, return_tensors="pt").to(
-        next(model.parameters()).device
-    )
-    input_ids, attention_mask = inp_tok["input_ids"], inp_tok["attention_mask"]
-    print(input_ids.shape,attention_mask.shape)
-    batch_size = input_ids.size(0)
+    inp_tok = tok(inp, padding=True, return_tensors="pt").to(next(model.parameters()).device)
 
-    # Setup storage of fast generation with attention caches.
-    # `cur_context` is used to define the range of inputs that are not yet
-    # stored in `past_key_values`. At each step, we are generating the
-    # next token for the index at `cur_context.stop + 1`.
-    past_key_values, cur_context = None, slice(0, attention_mask.sum(1).min().item())
+    # Define generation parameters
+    gen_args = {
+        'max_length': max_out_len, 
+        'do_sample': True, 
+        'top_k': top_k
+    }
 
-    with torch.no_grad():
-        while input_ids.size(1) < max_out_len:  # while not exceeding max output length
-            model_out = model(
-                input_ids=input_ids,
-                attention_mask=attention_mask
-                # past_key_values=past_key_values,
-                # use_cache=True,
-            )
-            # logits, past_key_values = model_out.logits, model_out.past_key_values
-            logits = model_out.logits
-            softmax_out = torch.nn.functional.softmax(logits[:, -1, :], dim=1)#没问题，因为slice了
+    out_tok = model.generate(inp_tok['input_ids'], **gen_args)
 
-            # Top-k sampling
-            tk = torch.topk(softmax_out, top_k, dim=1).indices
-            softmax_out_top_k = torch.gather(softmax_out, 1, tk)
-            softmax_out_top_k = softmax_out_top_k / softmax_out_top_k.sum(1)[:, None]
-            new_tok_indices = torch.multinomial(softmax_out_top_k, 1)
-            new_toks = torch.gather(tk, 1, new_tok_indices)
-
-            # If we're currently generating the continuation for the last token in `input_ids`,
-            # create a new index so we can insert the new token
-            if cur_context.stop == input_ids.size(1):
-                attention_mask = torch.cat(
-                    [attention_mask, attention_mask.new_zeros(batch_size, 1)], dim=1
-                )
-                input_ids = torch.cat(
-                    [input_ids,input_ids.new_ones(batch_size, 1) * tok.pad_token_id,],dim=1,
-                )
-
-            last_non_masked = attention_mask.sum(1) - 1
-            for i in range(batch_size):
-                new_idx = last_non_masked[i] + 1
-                if last_non_masked[i].item() + 1 != cur_context.stop:
-                    continue
-
-                # Stop generating if we've already maxed out for this prompt
-                if new_idx < max_out_len:
-                    input_ids[i][new_idx] = new_toks[i]
-                    attention_mask[i][new_idx] = 1
-
-            cur_context = slice(cur_context.stop, cur_context.stop + 1)
-
-    txt = [tok.decode(x) for x in input_ids.detach().cpu().numpy().tolist()]
+    # Decode and post-process text
+    txt = [tok.decode(x,skip_special_tokens = True) for x in out_tok.detach().cpu().numpy().tolist()]
     txt = [
         unicodedata.normalize("NFKD", x)
         .replace("\n\n", " ")
-        .replace("<|endoftext|>", "")
-        .replace("<unk>","")
-        .replace("<s>","")
-        .replace("</s>","")
         for x in txt
     ]
 
     return txt
+
+
+
+# def generate_fast(
+#     model: AutoModelForCausalLM,
+#     tok: AutoTokenizer,
+#     prompts: List[str],
+#     n_gen_per_prompt: int = 1,
+#     top_k: int = 5,
+#     max_out_len: int = 200,
+# ):
+#     """
+#     Fast, parallelized auto-regressive text generation with top-k sampling.
+#     Our custom implementation.
+#     """
+
+#     # Unroll prompts and tokenize
+#     inp = [prompt for prompt in prompts for _ in range(n_gen_per_prompt)]
+#     inp_tok = tok(inp, padding=True, return_tensors="pt").to(
+#         next(model.parameters()).device
+#     )
+#     input_ids, attention_mask = inp_tok["input_ids"], inp_tok["attention_mask"]
+#     print(input_ids.shape,attention_mask.shape)
+#     batch_size = input_ids.size(0)
+
+#     # Setup storage of fast generation with attention caches.
+#     # `cur_context` is used to define the range of inputs that are not yet
+#     # stored in `past_key_values`. At each step, we are generating the
+#     # next token for the index at `cur_context.stop + 1`.
+#     past_key_values, cur_context = None, slice(0, attention_mask.sum(1).min().item())
+
+#     with torch.no_grad():
+#         while input_ids.size(1) < max_out_len:  # while not exceeding max output length
+#             model_out = model(
+#                 input_ids=input_ids,
+#                 attention_mask=attention_mask
+#                 # past_key_values=past_key_values,
+#                 # use_cache=True,
+#             )
+#             # logits, past_key_values = model_out.logits, model_out.past_key_values
+#             logits = model_out.logits
+#             softmax_out = torch.nn.functional.softmax(logits[:, -1, :], dim=1)#没问题，因为slice了
+
+#             # Top-k sampling
+#             tk = torch.topk(softmax_out, top_k, dim=1).indices
+#             softmax_out_top_k = torch.gather(softmax_out, 1, tk)
+#             softmax_out_top_k = softmax_out_top_k / softmax_out_top_k.sum(1)[:, None]
+#             new_tok_indices = torch.multinomial(softmax_out_top_k, 1)
+#             new_toks = torch.gather(tk, 1, new_tok_indices)
+
+#             # If we're currently generating the continuation for the last token in `input_ids`,
+#             # create a new index so we can insert the new token
+#             if cur_context.stop == input_ids.size(1):
+#                 attention_mask = torch.cat(
+#                     [attention_mask, attention_mask.new_zeros(batch_size, 1)], dim=1
+#                 )
+#                 input_ids = torch.cat(
+#                     [input_ids,input_ids.new_ones(batch_size, 1) * tok.pad_token_id,],dim=1,
+#                 )
+
+#             last_non_masked = attention_mask.sum(1) - 1
+#             for i in range(batch_size):
+#                 new_idx = last_non_masked[i] + 1
+#                 if last_non_masked[i].item() + 1 != cur_context.stop:
+#                     continue
+
+#                 # Stop generating if we've already maxed out for this prompt
+#                 if new_idx < max_out_len:
+#                     input_ids[i][new_idx] = new_toks[i]
+#                     attention_mask[i][new_idx] = 1
+
+#             cur_context = slice(cur_context.stop, cur_context.stop + 1)
+
+#     txt = [tok.decode(x) for x in input_ids.detach().cpu().numpy().tolist()]
+#     txt = [
+#         unicodedata.normalize("NFKD", x)
+#         .replace("\n\n", " ")
+#         .replace("<|endoftext|>", "")
+#         .replace("<unk>","")
+#         .replace("<s>","")
+#         .replace("</s>","")
+#         for x in txt
+#     ]
+
+#     return txt
